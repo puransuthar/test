a1b60baebe78b36958ae9bfb6fd22b47c5eb2caf
puran suthar <puransuthar1993@gmail.com>
2021-10-16 19:00:52 +0530

    seed count updated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(\App\User::class, 2)->create();
+        factory(\App\User::class, 1000)->create();
     }
 }
